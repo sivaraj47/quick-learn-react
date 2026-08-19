7eaa71692ab476091212b75acc26e77c73798045
sivaraj47 <siva.avis47@gmail.com>
2026-08-19 19:22:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quick-learn-react
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sivaraj47/quick-learn-react)
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Welcome() {
+  return <h2 style={{ color: '#5C6AC4', fontSize: '22px' }}> Hello Siva</h2>;
+}
+export default Welcome;
