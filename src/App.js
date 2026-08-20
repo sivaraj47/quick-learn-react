2f27cdef4957933aabbcb804e5a8d84ee0518575
sivaraj47 <siva.avis47@gmail.com>
2026-08-20 16:35:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './style.css';
 function Address(props){
   return <h2>I am from chennai {props.add}</h2>
 }
+
+function Work(props){
+  return <h2>My job role is {props.role}</h2>
+}
 function Educ(props){
   return (
     <>
 <h2>I am studying {props.std}</h2>
 <h2>I got this quatly {props.mark}</h2>
 <h2>My School name {props.school}</h2>
+
 </>
   ); 
 }
@@ -26,6 +31,7 @@ export default function App() {
       <h1>welcome to {city}</h1>
       <Address add="guindy"/>
       <Educ std="10th standard" mark="80% mark" school="Bishop Herber HR School" />
+      <Work role="Senior Designer" />
     </div>
   );
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,4 +2,6 @@ import React from 'react';
 function Welcome() {
   return <h2 style={{ color: '#5C6AC4', fontSize: '22px' }}> Hello Siva</h2>;
 }
+
+
 export default Welcome;
